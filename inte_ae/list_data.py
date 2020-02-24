9306bd1e09f807c0b87bd566434e9565a5326b9e
erikvw <ew2789@gmail.com>
2020-02-24 07:18:31 -0600

    add additional ae_classifications to list data

--- inte_ae/list_data.py
+++ inte_ae/list_data.py
@@ -1,12 +1,12 @@
 from edc_constants.constants import OTHER, NOT_APPLICABLE, DEAD, UNKNOWN
 from edc_list_data import PreloadData
 
-
 list_data = {
     "edc_adverse_event.aeclassification": [
         ("anaemia", "Anaemia"),
         ("diarrhoea", "Diarrhoea"),
-        ("renal_impairment", "Renal impairment"),
+        ("pneumonia", "Pneumonia"),
+        ("TB", "TB"),
         (OTHER, "Other"),
     ],
     "edc_adverse_event.saereason": [
