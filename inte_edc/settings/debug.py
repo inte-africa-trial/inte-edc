84503e32ca5335ec6122c1cb404c725db4225dfc
erikvw <ew2789@gmail.com>
2020-06-08 14:01:55 -0500

    settings, crf schedule

--- inte_edc/settings/debug.py
+++ inte_edc/settings/debug.py
@@ -11,8 +11,8 @@ ALLOWED_HOSTS = [
     "bugamba.ug.inte.clinicedc.org",
     "localhost",
 ]
-# ETC_DIR = os.path.join(BASE_DIR, "tests", "etc")
-# KEY_PATH = os.path.join(ETC_DIR, "crypto_fields")
-# if os.path.exists(BASE_DIR) and not os.path.exists(KEY_PATH):
-#     os.makedirs(KEY_PATH)
-#     AUTO_CREATE_KEYS = True
+ETC_DIR = os.path.join(BASE_DIR, "tests", "etc")
+KEY_PATH = os.path.join(ETC_DIR, "crypto_fields")
+if os.path.exists(BASE_DIR) and not os.path.exists(KEY_PATH):
+    os.makedirs(KEY_PATH)
+    AUTO_CREATE_KEYS = True
