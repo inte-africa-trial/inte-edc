cd2e9d0fdc3f4c7fa789cf8af86373f674a0baf5
erikvw <ew2789@gmail.com>
2020-07-28 15:45:44 -0500

    fix missing other field

--- inte_subject/admin/other_baseline_data_admin.py
+++ inte_subject/admin/other_baseline_data_admin.py
@@ -20,7 +20,17 @@ class OtherBaselineDataAdmin(
         (None, {"fields": ("subject_visit", "report_datetime")}),
         ("Smoking", {"fields": ("smoking_status", "smoker_quit_ago")}),
         ("Alcohol", {"fields": ("alcohol", "alcohol_consumption")}),
-        ("Other", {"fields": ("employment_status", "education", "marital_status")}),
+        (
+            "Other",
+            {
+                "fields": (
+                    "employment_status",
+                    "employment_status_other",
+                    "education",
+                    "marital_status",
+                )
+            },
+        ),
         crf_status_fieldset_tuple,
         audit_fieldset_tuple,
     )
