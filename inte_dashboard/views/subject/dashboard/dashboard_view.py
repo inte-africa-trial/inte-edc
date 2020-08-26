9ad1354275a736bc3924ca3fe363439c60a95268
erikvw <ew2789@gmail.com>
2020-08-25 20:18:51 -0500

    add dashboard test to indicate where subject recruited from

--- inte_dashboard/views/subject/dashboard/dashboard_view.py
+++ inte_dashboard/views/subject/dashboard/dashboard_view.py
@@ -6,3 +6,11 @@ class DashboardView(SubjectDashboardView):
     consent_model = "inte_consent.subjectconsent"
     navbar_selected_item = "consented_subject"
     visit_model = "inte_subject.subjectvisit"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({"recruitment_clinic_name": self.get_recruitment_clinic_name()})
+        return context
+
+    def get_recruitment_clinic_name(self):
+        return self.consent.get_clinic_type_display()
