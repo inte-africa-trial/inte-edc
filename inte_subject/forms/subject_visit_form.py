c1dbff37ea9d14f021ca89807c388ed0b20fa1cc
erikvw <ew2789@gmail.com>
2020-08-25 20:21:13 -0500

    validate clinic services against visit code/seq

--- inte_subject/forms/subject_visit_form.py
+++ inte_subject/forms/subject_visit_form.py
@@ -1,5 +1,7 @@
+import pdb
+
 from django import forms
-from edc_constants.constants import OTHER
+from edc_constants.constants import OTHER, STUDY_DEFINED_TIMEPOINT
 from edc_form_validators import FormValidatorMixin
 from edc_sites.forms import SiteModelFormMixin
 from edc_visit_tracking.form_validators import VisitFormValidator
@@ -15,6 +17,21 @@ class SubjectVisitFormValidator(VisitFormValidator):
         self.m2m_other_specify(
             OTHER, m2m_field="clinic_services", field_other="clinic_services_other",
         )
+        selections = self.get_m2m_selected("clinic_services")
+        if (
+            self.cleaned_data.get("appointment").visit_code_sequence == 0
+            and STUDY_DEFINED_TIMEPOINT not in selections
+        ):
+            raise forms.ValidationError(
+                {"clinic_services": "This is scheduled study visit."}
+            )
+        elif (
+            self.cleaned_data.get("appointment").visit_code_sequence != 0
+            and STUDY_DEFINED_TIMEPOINT in selections
+        ):
+            raise forms.ValidationError(
+                {"clinic_services": "This is not a scheduled study visit."}
+            )
 
 
 class SubjectVisitForm(SiteModelFormMixin, FormValidatorMixin, forms.ModelForm):
