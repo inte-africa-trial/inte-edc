66fd4c92cb908bed128b5af278bb4553ffea6199
erikvw <ew2789@gmail.com>
2020-08-26 18:29:52 -0500

    add clinical review validation

--- inte_subject/forms/clinical_review_form.py
+++ inte_subject/forms/clinical_review_form.py
@@ -1,9 +1,16 @@
 from django import forms
+from edc_constants.constants import NO, NOT_APPLICABLE, YES
 from edc_form_validators.form_validator import FormValidator
+from inte_screening.constants import DIABETES_CLINIC, HIV_CLINIC, HYPERTENSION_CLINIC
 
-from ..models import ClinicalReview
+from ..models import (
+    ClinicalReview,
+    DiabetesInitialReview,
+    HivInitialReview,
+    HypertensionInitialReview,
+)
+from ..morbidity import Morbidities
 from .mixins import (
-    EstimatedDateFromAgoFormMixin,
     CrfModelFormMixin,
     CrfFormValidatorMixin,
 )
@@ -11,7 +18,68 @@ from .mixins import (
 
 class ClinicalReviewFormValidator(CrfFormValidatorMixin, FormValidator):
     def clean(self):
-        pass
+        morbidities = Morbidities(
+            subject_identifier=self.subject_identifier,
+            report_datetime=self.report_datetime,
+        )
+
+        clinic_type = self.subject_screening.clinic_type
+        if clinic_type == HYPERTENSION_CLINIC and not morbidities.is_hypertensive:
+            raise forms.ValidationError(
+                "Wait! Expected an Hypertension diagnosis at baseline. "
+                f"Was {HypertensionInitialReview._meta.verbose_name} CRF completed?"
+            )
+        elif clinic_type == DIABETES_CLINIC and not morbidities.is_diabetic:
+            raise forms.ValidationError(
+                "Wait! Expected a Diabetes diagnosis at baseline. "
+                f"Was {DiabetesInitialReview._meta.verbose_name} CRF completed?"
+            )
+        elif clinic_type == HIV_CLINIC and not morbidities.is_hiv_pos:
+            raise forms.ValidationError(
+                "Wait! Expected an HIV diagnosis at baseline. "
+                f"Was {HivInitialReview._meta.verbose_name} CRF completed?"
+            )
+
+        self.applicable_if_true(
+            not morbidities.is_hypertensive, field_applicable="hypertension_tested"
+        )
+        self.required_if(
+            YES, field="hypertension_tested", field_required="hypertension_tested_date"
+        )
+        self.required_if(
+            YES, field="hypertension_tested", field_required="hypertension_reason"
+        )
+        self.applicable_if(
+            YES, field="hypertension_tested", field_applicable="hypertension_dx"
+        )
+
+        self.applicable_if_true(
+            not morbidities.is_diabetic, field_applicable="diabetes_tested"
+        )
+        self.required_if(
+            YES, field="diabetes_tested", field_required="diabetes_tested_date"
+        )
+        self.required_if(YES, field="diabetes_tested", field_required="diabetes_reason")
+        self.applicable_if(YES, field="diabetes_tested", field_applicable="diabetes_dx")
+
+        self.applicable_if_true(
+            not morbidities.is_hiv_pos, field_applicable="hiv_tested"
+        )
+        self.required_if(YES, field="hiv_tested", field_required="hiv_tested_date")
+        self.required_if(YES, field="hiv_tested", field_required="hiv_reason")
+        self.applicable_if(YES, field="hiv_tested", field_applicable="hiv_dx")
+
+    def raise_if_dx_and_applicable(self, clinic, cond):
+        if self.subject_screening.clinic_type in [clinic] and self.cleaned_data.get(
+            f"{cond}_tested"
+        ) in [YES, NO]:
+            raise forms.ValidationError(
+                {
+                    f"{cond}_tested": (
+                        f"Not applicable. Patient was recruited from the {cond.title} clinic."
+                    ),
+                }
+            )
 
 
 class ClinicalReviewForm(CrfModelFormMixin, forms.ModelForm):
--- inte_subject/morbidity.py
+++ inte_subject/morbidity.py
@@ -9,21 +9,21 @@ class Morbidities:
         self.report_datetime = report_datetime
 
     @property
-    def is_hypertension(self):
-        return self._is_condition(HypertensionInitialReview)
+    def is_hypertensive(self):
+        return self._has_condition(HypertensionInitialReview)
 
     @property
     def is_hiv_pos(self):
-        return self._is_condition(HivInitialReview)
+        return self._has_condition(HivInitialReview)
 
     @property
-    def is_diabetes(self):
-        return self._is_condition(DiabetesInitialReview)
+    def is_diabetic(self):
+        return self._has_condition(DiabetesInitialReview)
 
-    def _is_condition(self, model_cls):
+    def _has_condition(self, model_cls):
         opts = dict(subject_visit__subject_identifier=self.subject_identifier)
         if self.report_datetime:
-            opts.update(subject_visit__report_datetime__lte=self.report_datetime)
+            opts.update(subject_visit__report_datetime__lt=self.report_datetime)
         try:
             model_cls.objects.get(**opts)
         except ObjectDoesNotExist:
