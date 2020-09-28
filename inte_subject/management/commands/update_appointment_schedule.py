0c051153096919ee66b785b11b50f7aa9e3c60f3
erikvw <ew2789@gmail.com>
2020-09-28 16:57:32 -0500

    add util to update appointment schedule for changes pre-6m

---
+++ inte_subject/management/commands/update_appointment_schedule.py
@@ -0,0 +1,11 @@
+from django.core.management import BaseCommand
+from inte_subject.update_appointment_schedule import update_appointment_schedule
+
+
+class Command(BaseCommand):
+
+    help = "Update appointments and subejctvisit after changes to pre-6m schedule"
+
+    def handle(self, *args, **options):
+
+        update_appointment_schedule()
