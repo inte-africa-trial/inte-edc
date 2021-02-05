4bbcc82621aeb37e02d95d84a262c16dff1590c0
erikvw <ew2789@gmail.com>
2021-02-04 20:34:15 -0600

    add manager to concrete class

--- inte_prn/models/subject_transfer.py
+++ inte_prn/models/subject_transfer.py
@@ -1,3 +1,4 @@
+from edc_action_item.models.action_model_mixin import ActionItemModelManager
 from edc_model import models as edc_models
 from edc_sites.models import CurrentSiteManager
 from edc_transfer.model_mixins import SubjectTransferModelMixin
@@ -9,6 +10,8 @@ class SubjectTransfer(
 ):
 
     on_site = CurrentSiteManager()
+
+    objects = ActionItemModelManager()
 
     class Meta(SubjectTransferModelMixin.Meta, edc_models.BaseUuidModel.Meta):
         pass
