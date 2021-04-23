6ace2a5711d562f9fc22707c8b1c03ef6e239f77
erikvw <ew2789@gmail.com>
2021-04-23 07:16:58 -0500

    accept a limited number of rows to add

--- inte_screening/extend_randomization_list.py
+++ inte_screening/extend_randomization_list.py
@@ -1,4 +1,5 @@
 import csv
+from typing import Optional
 
 from inte_sites.is_intervention_site import is_intervention_site
 from inte_sites.sites import all_sites
@@ -11,20 +12,21 @@ RandomizationListImporter(name='default', add=True, dryrun=False)
 """
 
 
-def randomization_rows(last_int):
+def randomization_rows(last_int: int, rows_to_add: int):
     rows = []
     for country, sites in all_sites.items():
         for site in sites:
             arm = "intervention" if is_intervention_site(site) else "control"
             n = int(f"{site.site_id}{last_int}")
-            for i in range(1, 51):
+            for i in range(1, rows_to_add + 1):
                 n += 1
                 rows.append([n, arm, site.name, country])
     return rows
 
 
-def export_to_file(filename, last_int):
+def export_to_file(filename: str, last_int: int, rows_to_add: Optional[int] = None):
+    rows_to_add = rows_to_add or 50
     with open(filename, "a") as f:
         writer = csv.writer(f)
-        for row in randomization_rows(last_int):
+        for row in randomization_rows(last_int, rows_to_add):
             writer.writerow(row)
