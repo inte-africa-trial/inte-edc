262cf2ec9ce0c0291af206ab97c4249e71311a73
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2021-06-15 12:13:06 +0100

    Fix failing test
    (test brought back into existence when Flake8 uncovered its test name had been shadowed by another test)

--- tests/inte_ae/test_death_report.py
+++ tests/inte_ae/test_death_report.py
@@ -68,7 +68,12 @@ class TestDeathReport(InteTestCaseMixin, TestCase):
             "subject_identifier": self.subject_consent.subject_identifier,
             "report_datetime": get_utcnow(),
             "death_date": get_utcnow() - relativedelta(days=5),
+            "death_location": "home",
+            "hospital_death": NO,
+            "informant": "spouse",
+            "confirmed_by": "tel",
             "comment": "",
+            "narrative": "some narrative",
         }
 
         form_validator = self.validate_form_validator(cleaned_data)
