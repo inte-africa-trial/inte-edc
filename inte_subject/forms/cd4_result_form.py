86c69b46c53dc4de92d60609eff6e5d941aa6f04
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2021-07-16 23:11:06 +0100

    Pass constants to validate_drawn_date_by_dx_date()

--- inte_subject/forms/cd4_result_form.py
+++ inte_subject/forms/cd4_result_form.py
@@ -1,6 +1,5 @@
-import pdb
-
 from django import forms
+from edc_constants.constants import HIV
 from edc_crf.modelform_mixins import CrfModelFormMixin
 from edc_form_validators.form_validator import FormValidator
 from edc_visit_schedule.utils import raise_if_baseline
@@ -15,7 +14,7 @@ class Cd4ResultFormValidator(ResultFormValidatorMixin, CrfFormValidatorMixin, Fo
     def clean(self):
         raise_if_baseline(self.cleaned_data.get("subject_visit"))
         raise_if_clinical_review_does_not_exist(self.cleaned_data.get("subject_visit"))
-        self.validate_drawn_date_by_dx_date("hiv", "HIV infection")
+        self.validate_drawn_date_by_dx_date(HIV, "HIV infection")
 
 
 class Cd4ResultForm(CrfModelFormMixin, forms.ModelForm):
